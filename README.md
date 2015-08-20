22933681692bb2635edefaed89ab37918e473da4
shaalx <1316011195@qq.com>
2015-08-20 17:25:04 +0800

    "download"

--- README.md
+++ README.md
@@ -28,24 +28,3 @@
 ![BS_bench](https://raw.githubusercontent.com/everfore/gotest/master/b2s/test.png "BS Benchmark")
 
 
-##	go1.5 build @windows
-
-[gcc-TMD](http://sourceforge.net/projects/tdm-gcc/)
-
-### env
-
-export CC=C:\tmd-gcc\bin\gcc.exe
-
-export CGO_ENABLED=1
-
-#build go1.5 with go1.4
-
-export GOROOT_BOOTSTRAP=go1.4
-
-export GOROOT=...
-
-export GOBIN=...
-
-export GOPATH=...
-
-PATH=%...%:$PATH
---
+++ download/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	// "bytes"
+	"time"
+)
+
+func checkerr(err error) bool {
+	if nil != err {
+		fmt.Println(err)
+		return true
+	}
+	return false
+}
+
+func main() {
+	go ticker()
+	file, err := os.OpenFile("master.zip", os.O_CREATE|os.O_WRONLY, 0644)
+	if checkerr(err) {
+		return
+	}
+	ch := get("https://codeload.github.com/shaalx/sstruct/zip/master", file)
+	// ch := get("https://codeload.github.com/everfore/push/zip/master", file)
+	fmt.Println(<-ch)
+}
+
+func ticker() {
+	cker := time.NewTicker(5e8)
+	for {
+		<-cker.C
+		fmt.Print(".")
+	}
+}
+
+func get(url_ string, file *os.File) chan bool {
+	ch := make(chan bool)
+	resp, err := http.Get(url_)
+	if checkerr(err) {
+		ch <- false
+		return ch
+	}
+	buf := make([]byte, 1024)
+	go func() {
+		defer func() {
+			file.Close()
+			ch <- true
+		}()
+		rd := bufio.NewReader(resp.Body)
+		for {
+			n, err := rd.Read(buf)
+			fmt.Print(n, "-")
+			m, werr := file.Write(buf[:n])
+			if checkerr(werr) {
+				break
+			}
+			fmt.Print(m, " ")
+			if err == io.EOF {
+				break
+			}
+
+		}
+		// io.Copy(file, resp.Body)
+	}()
+	return ch
+}
