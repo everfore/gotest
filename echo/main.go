8dfa4ff4f008099bf1972b67003bf56b8dc1b27f
shaalx <1316011195@qq.com>
2015-08-20 18:04:27 +0800

    "auto"

---
+++ echo/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	http.HandleFunc("/", echo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func echo(rw http.ResponseWriter, req *http.Request) {
+	URL, err := url.Parse(req.RequestURI)
+	fmt.Println(URL)
+	if checkerr(err) {
+		rw.Write([]byte(err.Error()))
+		return
+	}
+	site := URL.Query().Get("site")
+	fmt.Println(site)
+	if len(site) <= 1 {
+		return
+	}
+	resp, err := http.Get(site)
+	if checkerr(err) {
+		return
+	}
+	bs, err := ioutil.ReadAll(resp.Body)
+	if checkerr(err) {
+		return
+	}
+	rw.Write(bs)
+}
+
+func checkerr(err error) bool {
+	if nil != err {
+		fmt.Println(err)
+		return true
+	}
+	return false
+}
